fc6f81ccbbeb51c794ed15175de323e19034e5c8
Luca Fischer <luca.fischer@btc-ag.com>
2024-11-28 14:52:53 +0100

    removed a pringt in GuessNr

--- src/main/java/com/btcag/bootcamp/week1extra/GuessNumber.java
+++ src/main/java/com/btcag/bootcamp/week1extra/GuessNumber.java
@@ -17,8 +17,6 @@ public class GuessNumber {
         long maxNumb = bitShift - 1;
         long randNumber = random.nextLong(0, maxNumb);
 
-        // System.out.println(bitShift + " " + randNumber +  " " + maxNumb);
-
         while (guess != randNumber) {
             System.out.println(i + ". Versuch: Bitte eine Zahl im Bereich von 0 und " + maxNumb + " angeben: ");
             guess = input.nextLong();
